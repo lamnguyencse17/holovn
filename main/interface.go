b239d5b402fee852c14b75a83acbbc8d7cc50501
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-07-25 16:38:46 +0700

    Remove legacy main folder

--- main/interface.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type chatData struct {
-	Name      string `json:"name"`
-	Timestamp string `json:"timestamp"`
-	Message   string `json:"message"`
-	IsTl      bool   `json:"is_tl"`
-}
