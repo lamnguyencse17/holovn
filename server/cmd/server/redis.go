73dac74fe9a568fa02abe73e083df5e6db4f1f3b
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-07-28 13:42:13 +0700

    Redis Functions implemented

--- server/cmd/server/redis.go
+++ server/cmd/server/redis.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/go-redis/redis/v8"
+	"log"
 )
 
 
@@ -17,4 +18,35 @@ func initRedisClient (){
 		Password: ReadEnv("RedisPassword"),
 		DB:       0,
 	})
+}
+
+func SetKeyValue (key string, value string) bool{
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+func GetValue (key string) string{
+	value, err := rdb.Get(ctx, key).Result()
+	if err == redis.Nil {
+		log.Println(key, " does not exist")
+		return ""
+	} else if err != nil {
+		log.Println(err)
+		return ""
+	} else {
+		return value
+	}
+}
+
+func RemoveKey (key string) bool{
+	err := rdb.Del(ctx, key).Err()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
 }
