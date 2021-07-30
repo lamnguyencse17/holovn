268840aea31de165d3543cd7eaccede1bb2532be
TranHungKt <tranhung26122612@gmail.com>
2021-07-30 17:11:56 +0700

    Get schedule

--- server/cmd/server/getSchedule.go
+++ server/cmd/server/getSchedule.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"server/cmd/server/structure/channel"
 )
 
 const requestSchedulePrefix = "https://holodex.net/api/v2"
 const requestSchedulePostfix = "/live?lang=en&limit=25&max_upcoming_hours="
 
-func getSchedule(maxHours string, limit int) {
+func getSchedule(maxHours string, limit int) ([]channel.ChannelData, error){
 	client := http.DefaultClient
 	requestUrl := requestSchedulePrefix + requestSchedulePostfix + maxHours
 
 	resp, err := client.Get(requestUrl)
 
-	log.Println("REsponse", resp)
+	var parsedBody []channel.ChannelData
 
-	log.Println("err", err)
+	if err != nil {
+		return parsedBody , err
+	}
+
+	log.Println("log 1",parsedBody)
+
+	body, err:= ioutil.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		return parsedBody, err
+	}
+	log.Println("log 2",parsedBody)
+
+	err = json.Unmarshal(body, &parsedBody)
+
+	if err != nil {
+		return parsedBody, err
+	}
+	log.Println("log 3",parsedBody[0])
+	return parsedBody, nil
 }
--- server/cmd/server/structure/schedule/schedule.go
+++ server/cmd/server/structure/schedule/schedule.go
@@ -1,11 +1,14 @@
 package schedule
 
+import "server/cmd/server/structure/channel"
+
 type ScheduleData struct {
 	Id string `json:"id"`
 	Title string `json:"title"`
 	Type string `json:"type"`
 	PublishedAt string `json:"published_at"`
 	AvailableAt string `json:"available_at"`
-	Duration number `json:"duration"`
+	Duration int `json:"duration"`
 	Status string `json:"status"`
-}
+	Channel channel.ChannelData `json:"channel"`
+ }
