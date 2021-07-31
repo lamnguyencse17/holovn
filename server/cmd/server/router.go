f74064c499e642a06924d50350af77f7c7c3b89a
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-08-01 01:06:36 +0700

    Get Translations Handler Implemented And Connected

--- server/cmd/server/router.go
+++ server/cmd/server/router.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"server/cmd/server/liveRoom"
+	"server/cmd/server/routers"
 	"server/cmd/server/ws"
 )
 
 func runGinRouter(router *gin.Engine) {
-	router.GET("/live/:live_id", func(c *gin.Context) {
-		liveId := c.Param("live_id")
-		result, err := liveRoom.GetTl(liveId, 10000)
-		if err != nil {
-			c.String(http.StatusBadRequest, "Request cannot be handle")
-			return
-		}
-		c.JSON(http.StatusOK, result)
-	})
+	routers.RouteLive(router)
 	router.GET("/ws", func(c *gin.Context) {
 		ws.HandleWS(c.Writer, c.Request)
 	})
