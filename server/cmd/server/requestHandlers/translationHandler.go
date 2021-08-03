657e0a0d47219f2bb2d9913642cfd3b7b5ad5ed2
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-08-03 10:10:56 +0700

    Fixed Code Smells From Sonar Cloud
    Detail: https://sonarcloud.io/project/issues?id=lamnguyencse17_holovn&open=AXsJ-4N026TTZrG0E92b&pullRequest=23&resolved=false&types=CODE_SMELL

--- server/cmd/server/requestHandlers/translationHandler.go
+++ server/cmd/server/requestHandlers/translationHandler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"server/cmd/server/constants"
 	"server/cmd/server/models/translationStore"
 	"server/cmd/server/redis"
 )
@@ -17,7 +18,7 @@ func HandleGetTranslation(c *gin.Context) {
 		err := json.Unmarshal([]byte(unparsedTranslation), &translations)
 		if err != nil {
 			log.Println(err)
-			c.String(http.StatusBadRequest, "Request cannot be handled")
+			c.String(http.StatusBadRequest, constants.ERROR_HANDLING)
 			return
 		}
 		c.JSON(http.StatusOK, translations)
@@ -30,12 +31,12 @@ func HandleGetTranslation(c *gin.Context) {
 	stringifiedResult, err := json.Marshal(result)
 	if err != nil {
 		log.Println(err)
-		c.String(http.StatusBadRequest, "Request cannot be handled")
+		c.String(http.StatusBadRequest, constants.ERROR_HANDLING)
 		return
 	}
 	redis.SetKeyValueWithExpiration(liveId, string(stringifiedResult), 15)
 	if err != nil {
-		c.String(http.StatusBadRequest, "Request cannot be handled")
+		c.String(http.StatusBadRequest, constants.ERROR_HANDLING)
 		return
 	}
 	c.JSON(http.StatusOK, result)
